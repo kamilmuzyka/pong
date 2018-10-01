cc92ad450b3570d6a8ba145882d668edf937248d
kamilmuzyka <kamil.muzyka@gmail.com>
2018-10-01 23:31:26 +0200

    Update score. Add restart, pause, resume functions. Refactor

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
+
     /* 
-     * Initial Values
+     * Canvas
      */
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
@@ -10,49 +11,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const ch = canvas.height;
 
     /* 
-     * Elements
+     * Table
      */
-
-    // Dashes
     const dashWidth = cw * 0.006;
     const dashHeight = ch * 0.025;
 
-    // Ball
-    const ballSize = cw * 0.02;
-    let ballX = (cw / 2) - (ballSize / 2);
-    let ballY = (ch / 2) - (ballSize / 2);
-
-    const signs = [true, false];
-
-    function randomSign(signs) {
-        const index = Math.floor(Math.random() * signs.length);
-        return signs[index];
-    }
-
-    const signX = randomSign(signs) ? "+" : "-";
-    const signY = randomSign(signs) ? "+" : "-";
-
-    const velocityRateX = Number(signX + (Math.random() + 0.5) * 0.0013);
-    const velocityRateY = Number(signY + (Math.random() + 0.5) * 0.0013);
-    let velocityIncrease = 5;
-    let ballXV = cw * velocityRateX * velocityIncrease; // Velocity X
-    let ballYV = ch * velocityRateY * velocityIncrease; // Velocity Y
-
-    // Rackets
-    const racketWidth = cw * 0.02;
-    const racketHeight = ch * 0.2;
-
-    const playerX = cw * 0.07;
-    let playerY = (ch / 2) - (racketHeight / 2);
-
-    const computerX = cw - (cw * 0.07 + racketWidth);
-    let computerY = (ch / 2) - (racketHeight / 2);
-
-    /* 
-     * Functions
-     */
-
-    // Draw Table
     function table() {
         ctx.fillStyle = "#000000";
         ctx.fillRect(0, 0, cw, ch);
@@ -62,7 +25,14 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    // Draw Ball, Collision detection
+    /* 
+     * Ball
+     */
+    const ballSize = cw * 0.02;
+    let ballX;
+    let ballY;
+
+    // Draw ball
     function ball() {
         ctx.fillStyle = "#FFFFFF";
         ctx.fillRect(ballX, ballY, ballSize, ballSize);
@@ -70,18 +40,28 @@ document.addEventListener('DOMContentLoaded', function () {
         ballX += ballXV;
         ballY += ballYV;
 
+        checkCollision();
+    }
+
+    // Check ball's collision
+    function checkCollision() {
+
+        // Wall collision at X axis
+        if (ballX <= 0) {
+            computerScore++;
+            setBallFactors();
+        } else if (ballX + ballSize >= cw) {
+            userScore++;
+            setBallFactors();
+        }
+
+        // Wall collision at Y axis
         if (ballY <= 0 || ballY + ballSize >= ch) {
             ballYV = -ballYV;
             updateVelocity();
         }
 
-        // Ball-Wall collision at X axis
-        // if (ballX <= 0 || ballX + ballSize >= cw) {
-        //     ballXV = -ballXV;
-        //     updateVelocity();
-        // }
-
-        // Ball-Racket collision
+        // Collision with rackets
         if (
             ballX <= playerX + racketWidth &&
             ballX > playerX &&
@@ -103,20 +83,55 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    // Update velocity 
+    // Initial ball's direction and velocity
+    let directionX, directionY, velocityRateX, velocityRateY, ballXV, ballYV;
+    const velocityIncrease = 5;
+    const signs = [true, false];
+
+    function setBallFactors() {
+        ballX = (cw / 2) - (ballSize / 2);
+        ballY = (ch / 2) - (ballSize / 2);
+
+        directionX = signs[Math.round(Math.random())] ? "+" : "-";
+        directionY = signs[Math.round(Math.random())] ? "+" : "-";
+
+        velocityRateX = Number(directionX + (0.001 + Math.random() * 0.0004));
+        velocityRateY = Number(directionY + (0.001 + Math.random() * 0.0004));
+
+        ballXV = cw * velocityRateX * velocityIncrease;
+        ballYV = ch * velocityRateY * velocityIncrease;
+    }
+
+    setBallFactors();
+
+    // Update ball's velocity on collision
     function updateVelocity() {
         if (ballXV > 0 && ballXV < cw * 0.015) {
-            ballXV += cw * 0.00012;
+            ballXV += cw * 0.00025;
         } else if (ballXV < 0 && ballXV > -cw * 0.015) {
-            ballXV -= cw * 0.00012;
+            ballXV -= cw * 0.00025;
         }
 
         if (ballYV > 0 && ballYV < ch * 0.015) {
-            ballYV += ch * 0.00012;
+            ballYV += ch * 0.00025;
         } else if (ballYV < 0 && ballYV > -ch * 0.015) {
-            ballYV -= ch * 0.00012;
+            ballYV -= ch * 0.00025;
         }
     }
+
+    /* 
+     * Rackets
+     */
+    const racketWidth = cw * 0.02;
+    const racketHeight = ch * 0.2;
+
+    /* 
+     * Player's racket
+     */
+
+    // Player's X,Y
+    const playerX = cw * 0.07;
+    let playerY = (ch / 2) - (racketHeight / 2);
 
     // Draw player's racket
     function player() {
@@ -124,7 +139,7 @@ document.addEventListener('DOMContentLoaded', function () {
         ctx.fillRect(playerX, playerY, racketWidth, racketHeight);
     }
 
-    // Update player's racket position
+    // Update player's X,Y
     function playerPosition(e) {
         playerY = e.offsetY - racketHeight / 2;
 
@@ -135,14 +150,29 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    canvas.addEventListener('mousemove', playerPosition);
+
+    /* 
+     * Computer's racket
+     */
+
+    // Computer's X, Y
+    const computerX = cw - (cw * 0.07 + racketWidth);
+    let computerY = (ch / 2) - (racketHeight / 2);
+
     // Draw computer's racket
     function computer() {
         ctx.fillStyle = "#FFFFFF";
         ctx.fillRect(computerX, computerY, racketWidth, racketHeight);
     }
 
-    // Update computer's racket position
+    // Update computer's X,Y (Imitate AI)
     function computerPosition() {
+        if (computerY >= ch - racketHeight) {
+            computerY = ch - racketHeight;
+        } else if (computerY <= 0) {
+            computerY = 0;
+        }
         const middleComputerY = computerY + racketHeight / 2;
         const middleBallY = ballY + ballSize / 2;
         if (ballX > cw / 2) {
@@ -164,16 +194,50 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    /* 
+     * Game control
+     */
+
+    // Update score
+    let userScore = 0;
+    let computerScore = 0;
+
+    function updateScore() {
+        const fontSize = cw * 0.02;
+        ctx.font = `${fontSize}px 'Pixel'`;
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillText(userScore, cw / 4, ch / 4);
+        ctx.fillText(computerScore, cw - cw / 4, ch / 4);
+    }
+
     // Load game
     function game() {
-        requestAnimationFrame(game);
         table();
         ball();
         player();
         computer();
         computerPosition();
+        updateScore();
     }
 
-    canvas.addEventListener('mousemove', playerPosition);
-    game();
+    let gameInterval = setInterval(game, 1000 / 60);
+
+    // Restart game
+    function restart() {
+        ballX = (cw / 2) - (ballSize / 2);
+        ballY = (ch / 2) - (ballSize / 2);
+        userScore = 0;
+        computerScore = 0;
+        setBallFactors();
+    }
+
+    // Pause game
+    function pause() {
+        clearInterval(gameInterval);
+    }
+
+    // Resume game
+    function resume() {
+        gameInterval = setInterval(game, 1000 / 60);
+    }
 });
